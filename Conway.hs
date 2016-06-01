8dda9c132c979616806f2c4e5545c2da430e5733
Dustin Rohde <xdrohdex@gmail.com>
2016-06-01 15:03:07 -0400

    Clarified code.

--- Conway.hs
+++ Conway.hs
@@ -1,4 +1,4 @@
-module Conway (fromList, nextgen, survives, World, Cell, Point) where
+module Conway (fromList, nextgen, survives, Cell, Point) where
 
 import qualified Data.Set as Set
 import Data.Set (union, intersection, member)
@@ -7,52 +7,51 @@ import Data.Set (union, intersection, member)
 -- Public API.
 -- ---------------------------------------------------------------------------
 
-type World = Set.Set Cell
 type Cell = (Point, Point)
 type Point = Int
 
--- | Construct a World from a 2d list of 1's (living cell) and 0's (dead cell). 
-fromList :: [[Int]] -> World
+-- | Construct a Set of @Cell@s from a 2d list of 1's (living cells)
+-- and 0's (dead cells). 
+fromList :: [[Int]] -> Set.Set Cell
 fromList rows = Set.fromList $ concat cells
     where cells = [[(x, y) | (x, cell) <- iterIndices row, cell /= 0]
-                    | (y, row) <- iterIndices rows]
+                   | (y, row) <- iterIndices rows]
 
 -- | Generate the next iteration of the Game of Life.
-nextgen :: World -> World
-nextgen world = foldr f Set.empty (withAdjacent world)
-    where f cell cells = if cell `survives` world
-                              then Set.insert cell cells
-                              else cells 
+nextgen :: Set.Set Cell -> Set.Set Cell
+nextgen cells = foldr keepSurvivors Set.empty (withAdjacent cells)
+    where keepSurvivors cell newCells = if cell `survives` cells
+                                        then Set.insert cell newCells
+                                        else newCells
 
--- | Return True if, given a Cell and World, the given cell survives the
--- | next iteration.
-survives :: Cell -> World -> Bool
-survives cell world
+-- | Return True if, given a @Cell@ and a Set of @Cell@s,
+-- the given cell survives the next iteration.
+survives :: Cell -> Set.Set Cell -> Bool
+survives cell cells
   | liveNeighbors == 3 = True
-  | liveNeighbors == 2 = if cell `member` world
-                            then True
-                            else False
-  | otherwise = False
-    where neighbors = adjacentCells cell
-          liveNeighbors = length (world `intersection` neighbors)
+  | liveNeighbors == 2 = cell `member` cells
+  | otherwise          = False
+  where
+      neighbors = adjacentCells cell
+      liveNeighbors = length (cells `intersection` neighbors)
 
 
 -- Private API.
 -- ---------------------------------------------------------------------------
 
--- For every cell in a World, add all adjacent cells to the World.
-withAdjacent :: World -> World
+-- The union of a Set of @Cell@s with the Set of all its adjacent cells.
+withAdjacent :: Set.Set Cell -> Set.Set Cell
 withAdjacent = foldr f Set.empty
-    where f cell world = Set.insert cell world `union` adjacentCells cell
+    where f c cs = Set.insert c cs `union` adjacentCells c
 
--- Return the set of all adjacent cells to a given cell.
+-- The @Set@ of all adjacent @Cell@s to a given @Cell@.
 adjacentCells :: Cell -> Set.Set Cell
 adjacentCells cell = Set.map (vPlus cell) dirs
     where (x1, y1) `vPlus` (x2, y2) = (x1 + x2, y1 + y2)
           dirs = Set.fromList [(x, y) | x <- ds, y <- ds, x /= 0 || y /= 0]
           ds = [(-1) .. 1]
 
--- Return a list of pairs (i, x) where `i` is the index of item `x` in the
--- given list.
+-- The list of pairs (i, x) where each `i' is the index of each item `x'
+-- in the given list.
 iterIndices :: (Enum n, Num n) => [a] -> [(n, a)]
 iterIndices xs = zip [0..] xs
